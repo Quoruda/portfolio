81f27318d28e11e1b8c37571f452688fec8abbd4
Quoruda <audrick.soltner@proton.me>
2025-06-28 14:32:38 +0200

    Highlight.js

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import { createApp } from 'vue';
 import App from './App.vue';
 //import router from './router';
 import { ParticlesComponent } from "vue3-particles"  // Commenté temporairement
+import hljs from 'highlight.js';
+import 'highlight.js/styles/github.css'; // Choisissez un thème
 
 const app = createApp(App)
 //app.use(router);
 app.component('ParticlesBackground', ParticlesComponent)  // Commenté temporairement
 app.mount('#app')
+
+document.querySelectorAll('pre code').forEach((block) => {
+  hljs.highlightElement(block);
+});
--- vue.config.js
+++ vue.config.js
@@ -2,13 +2,4 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: "./",
-  chainWebpack: config => {
-    config.module
-        .rule('raw')
-        .test(/\.(txt|md)$/) // ← adapte selon ton besoin
-        .use('raw-loader')
-        .loader('raw-loader')
-        .end();
-  }
-
 })
