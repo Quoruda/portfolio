d6fdc7f348ddaad73bc17da6b87fb5e79091fde4
Quoruda <audrick.soltner@proton.me>
2025-11-04 01:14:42 +0100

    Add Application

---
+++ src/components/base/Application.vue
@@ -0,0 +1,32 @@
+<script setup>
+import ApplicationIcon from "./ApplicationIcon.vue";
+import Window from "./Window.vue";
+import {ref} from "vue";
+
+defineProps({
+  name: {
+    type: String,
+    required: true
+  },
+  icon: {
+    type: String,
+    required: true
+  }
+})
+
+const isOpen = ref(false);
+
+</script>
+
+<template>
+  <application-icon :name="name" :icon="icon" @click="isOpen=true"/>
+  <window v-if="isOpen" :name="name" :icon="icon" @close="isOpen=false">
+    <slot>
+
+    </slot>
+  </window>
+</template>
+
+<style scoped>
+
+</style>
