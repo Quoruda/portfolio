9fcddf416e71303fe66bc0c8a7f26417c0bcb130
Quoruda <audrick.soltner@proton.me>
2025-11-04 14:02:06 +0100

    Add container for icons

---
+++ src/components/base/IconsContainer.vue
@@ -0,0 +1,25 @@
+<script setup>
+</script>
+
+<template>
+  <div class="icons-container">
+    <slot>
+
+    </slot>
+  </div>
+</template>
+
+<style scoped>
+.icons-container{
+  flex: 1;
+  padding: 20px 40px 120px;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+  align-content: flex-start;
+  justify-content: center;
+  overflow-y: auto;
+  position: relative;
+  z-index: 1;
+}
+</style>
