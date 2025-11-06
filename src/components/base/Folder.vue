2df058a7ba7eae3542808d73770af10ee744ad76
Quoruda <audrick.soltner@proton.me>
2025-11-06 01:32:54 +0100

    Move Folder

---
+++ src/components/base/Folder.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+import Application from "./Application.vue";
+import IconsContainer from "./IconsContainer.vue";
+
+defineProps({
+  icon: String,
+  name: String
+});
+
+</script>
+
+<template>
+  <application :name="name" :icon="icon">
+    <IconsContainer>
+      <slot>
+
+      </slot>
+    </IconsContainer>
+  </application>
+</template>
+
+<style scoped>
+
+</style>
